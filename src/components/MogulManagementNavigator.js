03b79c463c29738f7ee6a9cc2c0aece501c88d04
Giovann Wah <giovann.wah92@gmail.com>
2020-07-19 22:18:38 -0400

    Updating home page style

--- src/components/MogulManagementNavigator.js
+++ src/components/MogulManagementNavigator.js
@@ -39,47 +39,87 @@ const handleClick = (path) => {
 
 const MogulManagementNavigator = props => (
   <div id="main-container">
-    <div id="quote-container">
-       <Quote
-         quote={ HomeContent.content.quote }
-         quotee={ HomeContent.content.quote_by }
-         keywords={ HomeContent.content.keywords }
-       />
-    </div>
-    <div id="secondary-container">
-      <div id="top-container">
-        <div className="top-column">
-          <IconTooltip className="tooltip-div-bottom" arrow title={ InvestorJSON.content.title } placement="top">
-            <IconButton onClick={() => handleClick("consultations/investor-consulting")}>
-              <img className="mogul-icon" id="mogul-logo-investor" src="../assets/Mogul-Management-Logo-04.svg" alt="Mogul Mangagement Investor Consulting"/>
-            </IconButton>
-          </IconTooltip>
-        </div>
-        <div className="top-column">
-          <IconTooltip className="tooltip-div-top" arrow title={ CreditJSON.content.title } placement="top">
-            <IconButton onClick={() => handleClick("consultations/credit-consulting")}>
-              <img className="mogul-icon" id="mogul-logo-credit" src="../assets/Mogul-Management-Logo-02.svg" alt="Mogul Mangagement Credit Repair"/>
-            </IconButton>
-          </IconTooltip>
-        </div>
-        <div className="top-column">
-          <IconTooltip className="tooltip-div-bottom" arrow title={ SellerJSON.content.title } placement="top">
-            <IconButton onClick={() => handleClick("consultations/motivated-seller")}>
-              <img className="mogul-icon" id="mogul-logo-seller" src="../assets/Mogul-Management-Logo-03.svg" alt="Mogul Mangagement Motivated Seller"/>
-            </IconButton>
-          </IconTooltip>
-        </div>
-      </div>
-      <div id="bottom-container">
-        <div>
-          <img id="mogul-logo-main" src="../assets/Mogul-Management-Logo-01.svg" alt="Mogul Mangagement Main"/>
-          <div id="learn-about-us"><Link to="/about">LEARN ABOUT US</Link></div>
-        </div>
+    <div id="mc-left-half">
+      <div id="quote-container">
+        <Quote
+          quote={ HomeContent.content.quote }
+          quotee={ HomeContent.content.quote_by }
+          keywords={ HomeContent.content.keywords }
+        />
       </div>
     </div>
-    <div id="quotee-container">
-      <p className="quote">{ ' ' }</p>
+    <div id="mc-right-half">
+      <div id="home-sep" />
+      <div id="secondary-container">
+        <div id="top-container">
+          <div className="top-column">
+            <IconTooltip className="tooltip-div-bottom" arrow title={ InvestorJSON.content.title } placement="top">
+              <IconButton onClick={() => handleClick("consultations/investor-consulting")}>
+                <img className="mogul-icon" id="mogul-logo-investor" src="../assets/Mogul-Management-Logo-04.svg" alt="Mogul Mangagement Investor Consulting"/>
+              </IconButton>
+            </IconTooltip>
+          </div>
+          <div className="top-column">
+            <IconTooltip className="tooltip-div-top" arrow title={ CreditJSON.content.title } placement="top">
+              <IconButton onClick={() => handleClick("consultations/credit-consulting")}>
+                <img className="mogul-icon" id="mogul-logo-credit" src="../assets/Mogul-Management-Logo-02.svg" alt="Mogul Mangagement Credit Repair"/>
+              </IconButton>
+            </IconTooltip>
+          </div>
+          <div className="top-column">
+            <IconTooltip className="tooltip-div-bottom" arrow title={ SellerJSON.content.title } placement="top">
+              <IconButton onClick={() => handleClick("consultations/motivated-seller")}>
+                <img className="mogul-icon" id="mogul-logo-seller" src="../assets/Mogul-Management-Logo-03.svg" alt="Mogul Mangagement Motivated Seller"/>
+              </IconButton>
+            </IconTooltip>
+          </div>
+        </div>
+        <div id="bottom-container">
+          <div>
+            <img id="mogul-logo-main" src="../assets/Mogul-Management-Logo-01.svg" alt="Mogul Mangagement Main"/>
+            <div id="learn-about-us"><Link to="/about">LEARN ABOUT US</Link></div>
+          </div>
+        </div>
+      </div>
     </div>
+    {/*<div id="quote-container">*/}
+    {/*   <Quote*/}
+    {/*     quote={ HomeContent.content.quote }*/}
+    {/*     quotee={ HomeContent.content.quote_by }*/}
+    {/*     keywords={ HomeContent.content.keywords }*/}
+    {/*   />*/}
+    {/*</div>*/}
+    {/*<div id="secondary-container">*/}
+    {/*  <div id="top-container">*/}
+    {/*    <div className="top-column">*/}
+    {/*      <IconTooltip className="tooltip-div-bottom" arrow title={ InvestorJSON.content.title } placement="top">*/}
+    {/*        <IconButton onClick={() => handleClick("consultations/investor-consulting")}>*/}
+    {/*          <img className="mogul-icon" id="mogul-logo-investor" src="../assets/Mogul-Management-Logo-04.svg" alt="Mogul Mangagement Investor Consulting"/>*/}
+    {/*        </IconButton>*/}
+    {/*      </IconTooltip>*/}
+    {/*    </div>*/}
+    {/*    <div className="top-column">*/}
+    {/*      <IconTooltip className="tooltip-div-top" arrow title={ CreditJSON.content.title } placement="top">*/}
+    {/*        <IconButton onClick={() => handleClick("consultations/credit-consulting")}>*/}
+    {/*          <img className="mogul-icon" id="mogul-logo-credit" src="../assets/Mogul-Management-Logo-02.svg" alt="Mogul Mangagement Credit Repair"/>*/}
+    {/*        </IconButton>*/}
+    {/*      </IconTooltip>*/}
+    {/*    </div>*/}
+    {/*    <div className="top-column">*/}
+    {/*      <IconTooltip className="tooltip-div-bottom" arrow title={ SellerJSON.content.title } placement="top">*/}
+    {/*        <IconButton onClick={() => handleClick("consultations/motivated-seller")}>*/}
+    {/*          <img className="mogul-icon" id="mogul-logo-seller" src="../assets/Mogul-Management-Logo-03.svg" alt="Mogul Mangagement Motivated Seller"/>*/}
+    {/*        </IconButton>*/}
+    {/*      </IconTooltip>*/}
+    {/*    </div>*/}
+    {/*  </div>*/}
+    {/*  <div id="bottom-container">*/}
+    {/*    <div>*/}
+    {/*      <img id="mogul-logo-main" src="../assets/Mogul-Management-Logo-01.svg" alt="Mogul Mangagement Main"/>*/}
+    {/*      <div id="learn-about-us"><Link to="/about">LEARN ABOUT US</Link></div>*/}
+    {/*    </div>*/}
+    {/*  </div>*/}
+    {/*</div>*/}
   </div>
 );
 
