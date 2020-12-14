d1d6b5f10938c6c83ae796726db2fb6a24ad3e5d
Giovann Wah <giovann.wah92@gmail.com>
2020-12-13 22:30:28 -0500

    Funding style update

--- src/pages/consultations/funding.js
+++ src/pages/consultations/funding.js
@@ -63,33 +63,38 @@ const Funding = props => {
             </div>
           </div>
         </div>
-        <Table className="funding-table">
-          <TableHead className="funding-table" onClick={() => setOpen(!open)}>
-            <h3 style={{cursor: "pointer"}} className="funding-list-title"><a>List of Products Offered to Our Clients</a></h3>
-          </TableHead>
-          <TableBody className="funding-table">
-            <TableRow className="funding-table">
-              <TableCell className="funding-table">
-                <Collapse in={open} timeout="auto" unmountOnExit className="funding-table">
-                  <ul className="funding-list page-paragraph" id="funding-list-2">
-                    {
-                      JSONFundingPageContent.content.list_2.map((item) => (
-                        <li>{item}</li>
-                      ))
-                    }
-                  </ul>
-                </Collapse>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-        <div id="funding-consul-link">
-          <h3 className="funding-list-title">
-            <a href="https://share.hsforms.com/1qnitGOUCQMa4mM4cgvF_SQ45jf7" target="_blank">Click here for your free consultation!</a>
-          </h3>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <Table className="funding-table">
+                <TableHead className="funding-table" onClick={() => setOpen(!open)}>
+                  <h3 style={{cursor: "pointer", margin: 0}} className="funding-list-title"><a>List of Products Offered to Our Clients</a></h3>
+                </TableHead>
+                <TableBody className="funding-table">
+                  <TableRow className="funding-table">
+                    <TableCell className="funding-table">
+                      <Collapse in={open} timeout="auto" unmountOnExit className="funding-table">
+                        <ul className="funding-list page-paragraph" id="funding-list-2">
+                          {
+                            JSONFundingPageContent.content.list_2.map((item) => (
+                              <li>{item}</li>
+                            ))
+                          }
+                        </ul>
+                      </Collapse>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+              <div id="funding-consul-link">
+                <h3 className="funding-list-title">
+                  <a href="https://share.hsforms.com/1qnitGOUCQMa4mM4cgvF_SQ45jf7" target="_blank">Click here for your free consultation!</a>
+                </h3>
+              </div>
+            </div>
+          </div>
         </div>
         <div id="funding-pdf">
-
           <Document
             file={pdfFile}
             id="funding-doc"
